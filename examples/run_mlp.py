cb506fb2258c4edcf84a4ff0814dfd0f7cc5fde1
abe5240 <112660967+abe5240@users.noreply.github.com>
2025-03-14 14:33:55 -0400

    Update run_mlp.py [no ci]

--- examples/run_mlp.py
+++ examples/run_mlp.py
@@ -2,12 +2,32 @@ import time
 import math
 import torch
 import orion
-import models
 from orion.core.utils import (
     get_mnist_datasets,
     mae, 
     train_on_mnist
 )
+
+class MLP(on.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.flatten = on.Flatten()
+        
+        self.fc1 = on.Linear(784, 128)
+        self.bn1 = on.BatchNorm1d(128)
+        self.act1 = on.Quad()
+        
+        self.fc2 = on.Linear(128, 128)
+        self.bn2 = on.BatchNorm1d(128)
+        self.act2 = on.Quad() 
+        
+        self.fc3 = on.Linear(128, num_classes)
+
+    def forward(self, x): 
+        x = self.flatten(x)
+        x = self.act1(self.bn1(self.fc1(x)))
+        x = self.act2(self.bn2(self.fc2(x)))
+        return self.fc3(x)
 
 # Set seed for reproducibility
 torch.manual_seed(42)
@@ -17,7 +37,7 @@ scheme = orion.init_scheme("../configs/mlp.yaml")
 batch_size = scheme.get_batch_size()
 trainloader, testloader = get_mnist_datasets(
     data_dir="../data", batch_size=batch_size)
-net = models.MLP()
+net = MLP()
 
 # Train model (optional)
 train_on_mnist(net, data_dir="../data", epochs=1, device="cuda")
